33e22bdbcef7500176cdaab4acdfe62113b9e66d
Shiba-You <s.y.chocola2921@gmail.com>
2025-04-29 17:14:16 +0900

    fix

--- backend/src/openid.js
+++ backend/src/openid.js
@@ -3,14 +3,16 @@ const { Issuer } = require("openid-client");
 let client;
 async function getClient() {
   if (client) return client;
+  const redirect_uris =
+    process.env.NODE_ENV == "production"
+      ? `${process.env.BACKEND_URL}/api/auth/mypage`
+      : `${process.env.BACKEND_URL}:${process.env.PORT}/api/auth/mypage`;
 
   const issuer = await Issuer.discover(process.env.IDP_URL);
   client = new issuer.Client({
     client_id: process.env.CLIENT_ID,
     client_secret: process.env.CLIENT_SECRET,
-    redirect_uris: [
-      `${process.env.BACKEND_URL}:${process.env.PORT}/api/auth/mypage`,
-    ],
+    redirect_uris: [redirect_uris],
     response_types: ["code"],
   });
 }
