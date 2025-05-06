2c1d83c3448fd13a85d8b523fd50397ed201c42d
Shiba-You <s.y.chocola2921@gmail.com>
2025-05-06 18:08:59 +0900

    add wasm

--- terraform/install_nginx.sh
+++ terraform/install_nginx.sh
@@ -8,8 +8,9 @@ sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 
 # docker compose インストール用
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
-  -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose -v
+
+
+git clone https://github.com/Shiba-You/qiita-clone.git
